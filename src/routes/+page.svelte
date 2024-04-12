33a61f4158ef9790911236d1410e7bd9b5e1e187
Nurul <njagirdar@virginmedia.com>
2024-04-12 01:24:52 +0100

    api call working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
 <script>
-  import Search from "../lib/components/Search.svelte";
+  import { onMount } from "svelte";
 
   let location = "Leeds";
   let weather = null;
+
+  async function fetchWeather(location) {
+    const apiKey = import.meta.env.VITE_NEXT_PUBLIC_WEATHER_API_KEY;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3&aqi=no&alerts=no`;
+    const response = await fetch(url);
+    weather = await response.json();
+  }
+
+  onMount(() => {
+    fetchWeather(location);
+  });
 </script>
 
 <div class="flex flex-col bg-black items-center p-4 text-white min-h-screen">
-  <Search />
+  <h1>{weather ? weather.location.region : "Loading"}</h1>
 </div>
